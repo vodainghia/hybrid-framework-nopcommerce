c98c3f18b29344cd1681f6960967de550b19bd27
Nghia Vo <devnghiavo@gmail.com>
2021-07-13 00:44:54 +0700

    Init new automation test framework

---
+++ testcases/com/bankguru/payment/Payment_01_Fund_Transfer.java
@@ -0,0 +1,21 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Payment_01_Fund_Transfer {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
---
+++ testcases/com/bankguru/payment/Payment_02_Deposit.java
@@ -0,0 +1,21 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Payment_02_Deposit {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void f() {
+	}
+
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
